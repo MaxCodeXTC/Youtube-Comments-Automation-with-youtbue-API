af0714f9c6c68567145562effa4c3aeffdba91a0
Hazrat Ali <57933210+hazratali-bit@users.noreply.github.com>
2020-05-10 15:24:43 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+# these are the two modules that are essential while using the google api do not worry to much about it.
+
+from apiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+
